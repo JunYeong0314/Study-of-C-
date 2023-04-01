24446860daab81aae5624a6b59041fdf83a18e62
JunYeong0314 <96372487+JunYeong0314@users.noreply.github.com>
2023-04-01 13:53:00 +0900

    Add files via upload
    전치행렬 알고리즘

---
+++ 자료구조/Matrix/Matrix/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "SparseMatrix.h"
+
+using namespace std;
+
+int main() {
+    SparseMatrix A(3, 3, 0);
+    A.NewTerm(0, 0, 1);
+    A.NewTerm(1, 1, 2);
+    A.NewTerm(2, 2, 3);
+
+    // Print the matrix
+    cout << "A:" << endl;
+    cout << A << endl;
+
+    // Transpose the matrix and print it
+    SparseMatrix B = A.Transpose();
+    cout << "B:" << endl;
+    cout << B << endl;
+
+    return 0;
+}
